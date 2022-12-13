ea729f647b93d147da3f42bacd64655ead0c25ed
edmond-dev <edmondprojects71@gmail>
2022-12-13 01:05:45 -0700

    webapp ecommerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
+  const url = "http://127.0.0.1:8000/products"
 function App() {
-  return (
+    // eslint-disable-next-line no-undef
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false)
+
+    // eslint-disable-next-line no-undef
+     useEffect(() => {
+    axios.get(url).then((response) => {
+      setProducts(response.data);
+    }).catch(e => setError(e));
+  }, []);
+
+    console.log(error)
+  const home = () => (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>CSC-480 Ecommerce</div>
       </header>
+        <main>
+            <div className="search-bar-container">
+                <div className="search-bar"><input name="searchProduct" type={"text"} placeholder={'search products'}/></div>
+            </div>
+            <div className="products-display-container">
+                <div className="products-display">
+                    <div className="products-display-title">Buy from a select of products</div>
+                        <div className="grid-product-display">
+                            {products.map((p) =>(
+                                <div className="product-container">
+                                    <div className="product">
+                                        <div>{p.id}</div>
+                                        <div>{p.name}</div>
+                                        <div>{p.desc}</div>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div className="footer-container">
+                    <div className="footer-main">
+                        <div> Copyright 2022. Senior year project.</div>
+                        <div>The College of Idaho</div>
+                    </div>
+                </div>
+        </footer>
     </div>
   );
+    return (
+    <>
+     {home()}
+    </>
+)
 }
+
+
 
 export default App;
